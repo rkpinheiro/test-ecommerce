7af22ac60bf2242b0588701e9d8c31d70af47f71
Rurik Pinheiro <rurikspin@gmail.com>
2018-08-11 11:54:42 -0300

    Add login form

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Ecoomerce - @yield('title', 'Home')</title>
 
@@ -37,10 +38,32 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li>
+                <li><a href="{{route('store.home')}}">Home</a></li>
                 <li><a href="#">Products</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (! Auth::guest())
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
                 <li><a href="#">Cart</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
